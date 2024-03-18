719a92c3d9fc423e931f1d1938c9194c69806f67
robermadata <68168618+robermadata@users.noreply.github.com>
2024-03-18 08:54:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -19,19 +19,34 @@ function imageLoaded() {
   }
 }
 
+function setAttributes(element, attributes) {
+  for (let key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 function displayPhotos() {
   imagesLoaded = 0;
   totalImages = photosArray.length;
   console.log("total images", totalImages);
   photosArray.forEach((photo) => {
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank"
+    });
+    //item.setAttribute("href", photo.links.html);
+    //item.setAttribute("target", "_blank");
 
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description
+    });
+    //img.setAttribute("src", photo.urls./regular);
+    //img.setAttribute("alt", photo.alt_description);
+    //img.setAttribute("title", photo.alt_description);
 
     img.addEventListener("load", imageLoaded);
     item.appendChild(img);
